f58ab9ac7ccf700bfef3e13d0773c4b325273da6
vamsikrishna5737 <98752676+vamsikrishna5737@users.noreply.github.com>
2022-04-07 23:47:29 +0530

    Updates

---
+++ precache-manifest.0f3a95dc1d2ce5d1434af695ef50318a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3279ab00499b88d8f493b7b6d9db92",
+    "url": "/groceries/index.html"
+  },
+  {
+    "revision": "895fa9fafcb0b6796c92",
+    "url": "/groceries/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/groceries/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/groceries/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895fa9fafcb0b6796c92",
+    "url": "/groceries/static/js/main.06c69a26.chunk.js"
+  },
+  {
+    "revision": "9f8cf024105ee7efd216",
+    "url": "/groceries/static/js/runtime-main.bae2700d.js"
+  }
+]);
